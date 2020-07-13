35c9c805c540fbfe34c49f0c3c7ee2cb60162aa3
ffranco4 <52253626+ffranco4@users.noreply.github.com>
2020-07-13 14:51:31 -0500

    I wanna be blue
    SIngle app

--- I wanna be blue/ViewController.swift
+++ I wanna be blue/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var onColor = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var ColorLabel: UILabel!
+    
+    @IBAction func WannaBeBluePressed(_ sender: Any){
+        
+        onColor = !onColor
+        UpdateUI()
+    }
+    
+    @IBOutlet weak var BlueBtnLabel: UIButton!
+    
+    
+    func UpdateUI() {
+        if onColor {
+            view.backgroundColor = .white
+            BlueBtnLabel.setTitle("I wanna be blue", for: .normal)
+            ColorLabel.text = "what color am I?"
+            ColorLabel.textColor = .purple
+        } else {
+            view.backgroundColor = .blue
+            BlueBtnLabel.setTitle("I am blue now", for: .normal)
+            ColorLabel.text = "Its not easy being green"
+            ColorLabel.textColor = .green}
+    }
+    
 }
+
 
